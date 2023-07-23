559d88deb1828d85584b44c09d1a4407297995ff
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-07-23 14:49:17 +0530

    getUser updated

--- backend/controller/getUser.js
+++ backend/controller/getUser.js
@@ -1,9 +1,22 @@
 const User = require("../models/User");
+
 exports.getUser = async (req, res) => {
+  const { email } = req.body; // Assuming email and password are sent in the request body
+
   try {
-    const userData = await User.find({});
+    // Check if the provided email and password match any user in the database
+    const userData = await User.findOne({ email });
+
+    if (!userData) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found or invalid credentials",
+      });
+    }
+   
+
     res.json({ success: true, data: userData });
   } catch (error) {
-    res.status(500).json({ success: false, error: error });
+    res.status(500).json({ success: false, error: error.message });
   }
 };
