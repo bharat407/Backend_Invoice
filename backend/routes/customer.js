520d66b52d642f6980f543168595484efae87f92
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-07-27 11:26:35 +0530

    Customer Complted [CRUD] opertaion Some basic upadation pending for frontend

--- backend/routes/customer.js
+++ backend/routes/customer.js
@@ -13,44 +13,52 @@ router.post("/createCustomer", createCustomer);
 
 router.get("/getallCustomer", getCustomer);
 
-router.delete("/deleteCustomer", async (req, res) => {
-  const { email } = req.body;
+// Delete Customer by Email
+router.delete("/deleteCustomer/:email", async (req, res) => {
+  const customerEmail = req.params.email;
   try {
-    await AddCustomer.deleteOne({ email: { $in: email } });
-    // Send a success response
-    // Customer was found and deleted successfully
-    res
-      .status(200)
-      .json({ success: true, message: "Customers deleted successfully." });
+    const deletedCustomer = await AddCustomer.findOneAndDelete({
+      email: customerEmail,
+    });
+    if (deletedCustomer) {
+      console.log("Deleted customer:", deletedCustomer);
+      res
+        .status(200)
+        .json({ success: true, message: "Customer deleted successfully." });
+    } else {
+      console.log("Customer not found");
+      res.status(404).json({ success: false, message: "Customer not found." });
+    }
   } catch (error) {
-    // If there's an error during deletion, send an error response
-    console.error("Error deleting customers:", error);
+    console.error("Error deleting customer:", error);
     res
       .status(500)
-      .json({ success: false, message: "Failed to delete customers." });
+      .json({ success: false, message: "Failed to delete customer." });
   }
 });
 
-// Customer with the given ID was Update or not found
-
-router.put("/updateCustomer/:email", (req, res) => {
+// Update Customer by Email
+router.put("/updateCustomer/:email", async (req, res) => {
   const customerEmail = req.params.email;
   console.log("Customer Email:", customerEmail);
   console.log("Update data:", req.body);
-  Customer.findOneAndUpdate({ email: customerEmail }, req.body, { new: true })
-    .then((updatedCustomer) => {
-      if (updatedCustomer) {
-        console.log("Updated customer:", updatedCustomer);
-        res.status(200).json(updatedCustomer);
-      } else {
-        console.log("Customer not found");
-        res.status(404).json({ error: "Customer not found" });
-      }
-    })
-    .catch((err) => {
-      console.error("Error updating customer:", err);
-      res.status(500).json({ error: "Error updating customer" });
-    });
+  try {
+    const updatedCustomer = await Customer.findOneAndUpdate(
+      { email: customerEmail },
+      req.body,
+      { new: true }
+    );
+    if (updatedCustomer) {
+      console.log("Updated customer:", updatedCustomer);
+      res.status(200).json(updatedCustomer);
+    } else {
+      console.log("Customer not found");
+      res.status(404).json({ error: "Customer not found" });
+    }
+  } catch (err) {
+    console.error("Error updating customer:", err);
+    res.status(500).json({ error: "Error updating customer" });
+  }
 });
 
 module.exports = router;
